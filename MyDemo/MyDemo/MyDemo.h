09f284bdc2916fb6ea337ff07f138a9f2cc0d49c
apple <sunchongxiawork@163.com>
2016-09-07 10:25:36 +0800

    增加了两个类

---
+++ MyDemo/MyDemo/MyDemo.h
@@ -0,0 +1,13 @@
+//
+//  MyDemo.h
+//  MyDemo
+//
+//  Created by apple on 16/9/7.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyDemo : UIView
+
+@end
---
+++ MyDemo/MyDemo/MyDemo.m
@@ -0,0 +1,21 @@
+//
+//  MyDemo.m
+//  MyDemo
+//
+//  Created by apple on 16/9/7.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "MyDemo.h"
+
+@implementation MyDemo
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
